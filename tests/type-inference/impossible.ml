a29e3386371b069747f62be3a6dd944d149c4dc3
Jeff Smits <jeff.smits@gmail.com>
2014-08-07 20:18:06 +0200

    Added another example. Made sure the impossible example doesn't have the solver try to build an infinite type

--- tests/type-inference/impossible.ml
+++ tests/type-inference/impossible.ml
@@ -1,1 +1,3 @@
+(* This is an example of undecidability of polymorphic recursion
+(which isn't even in CamlLight, but it's still a nice example) *)
 let rec diverge x = diverge diverge x;;
---
+++ tests/type-inference/useless.ml
@@ -0,0 +1,2 @@
+(* This should type-check, even if it doesn't give you a useful function *)
+let rec useless x = useless x;;
